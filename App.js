10f5c30bd8eb5d9222e18a432c9b61447d668d50
Santhosh-priya <santhoshpriya29052004@gmail.com>
2024-10-24 15:16:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Navbar from './pages/navbar';
+import Homepage from './pages/homepage';
+import Footer from './pages/footer';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Homepage Style="margin-top:0px "/>
+      <Footer/>
+    </div>
+
+  );
+}
+
+export default App;
